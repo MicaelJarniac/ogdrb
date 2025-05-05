1d34affc6e57d935a8e16b4d4807b62d39b5b76c
Micael Jarniac <micael@jarniac.dev>
2025-05-05 14:28:03 -0300

    feat: table

--- src/ogdrb/main.py
+++ src/ogdrb/main.py
@@ -4,11 +4,37 @@ from __future__ import annotations
 
 __all__: tuple[str, ...] = ()
 
+from typing import TYPE_CHECKING, TypedDict
+
 from nicegui import ui
+
+if TYPE_CHECKING:  # pragma: no cover
+    from nicegui.events import GenericEventArguments
+
+
+class Circle(TypedDict):
+    """Circle class for Leaflet map."""
+
+    id: int
+    lat: float
+    lng: float
+    radius: float
+
+
+class ZoneRow(TypedDict):
+    """ZoneRow class for AgGrid."""
+
+    id: int
+    name: str
+    lat: float
+    lng: float
+    radius: float
 
 
 @ui.page("/")
 async def index() -> None:
+    rows: list[ZoneRow] = []
+
     # Leaflet map with circle-only draw toolbar
     m = ui.leaflet(
         center=(0.0, 0.0),
@@ -27,9 +53,9 @@ async def index() -> None:
         },
     ).classes("w-full h-96")
 
-    # programmatic circle
+    # Programmatic circle
     # https://github.com/zauberzeug/nicegui/discussions/4644
-    async def add_circle() -> None:
+    async def add_circle(lat: float, lng: float, radius: float) -> None:
         id_ = await ui.run_javascript(
             f"""
             const out = [];
@@ -37,7 +63,7 @@ async def index() -> None:
             map.eachLayer(layer => {{
                 if (layer instanceof L.FeatureGroup) {{
                     const myCircle = L.circle(
-                        [51.49, -0.095], {{ radius: 200 }}
+                        [{lat}, {lng}], {{ radius: {radius} }}
                     ).addTo(layer);
                     out.push(myCircle);
                 }}
@@ -48,8 +74,8 @@ async def index() -> None:
         )
         ui.notify(f"Programmatic circle added (id: {id_})")
 
-    async def list_circles() -> None:
-        circles = await ui.run_javascript(
+    async def list_circles() -> list[Circle]:
+        circles: list[Circle] = await ui.run_javascript(
             f"""
             const out = [];
             getElement('{m.id}').map.eachLayer(layer => {{
@@ -68,9 +94,44 @@ async def index() -> None:
             timeout=1.0,
         )
         ui.notify(f"All circles: {circles}")
+        return circles
 
-    ui.button("Add circle", on_click=add_circle)
-    ui.button("List all circles", on_click=list_circles)
+    columns = [
+        {"field": "name", "headerName": "Name", "editable": True},
+        {"field": "lat", "headerName": "Latitude", "editable": True},
+        {"field": "lng", "headerName": "Longitude", "editable": True},
+        {"field": "radius", "headerName": "Radius", "editable": True},
+        {"field": "id", "headerName": "ID"},
+    ]
+
+    def add_row() -> None:
+        new_id = max((dx["id"] for dx in rows), default=-1) + 1
+        rows.append(ZoneRow(id=new_id, name="New zone", lat=0.0, lng=0.0, radius=0.0))
+        ui.notify(f"Added row with ID {new_id}")
+        aggrid.update()
+
+    def handle_cell_value_change(e: GenericEventArguments) -> None:
+        new_row: ZoneRow = e.args["data"]
+        ui.notify(f"Updated row to: {e.args['data']}")
+        rows[:] = [row | new_row if row["id"] == new_row["id"] else row for row in rows]
+
+    async def delete_selected() -> None:
+        selected_id = [row["id"] for row in await aggrid.get_selected_rows()]
+        rows[:] = [row for row in rows if row["id"] not in selected_id]
+        ui.notify(f"Deleted row with ID {selected_id}")
+        aggrid.update()
+
+    aggrid = ui.aggrid(
+        {
+            "columnDefs": columns,
+            "rowData": rows,
+            "rowSelection": "multiple",
+            "stopEditingWhenCellsLoseFocus": True,
+        }
+    ).on("cellValueChanged", handle_cell_value_change)
+
+    ui.button("Delete selected zones", on_click=delete_selected)
+    ui.button("New zone", on_click=add_row)
 
 
 if __name__ in {"__main__", "__mp_main__"}:
