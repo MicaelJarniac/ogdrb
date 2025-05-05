a2b44e53ea74e1eca44c9ad91d43322cb27624ef
Micael Jarniac <micael@jarniac.dev>
2025-05-05 12:20:33 -0300

    feat: add circle

--- src/ogdrb/main.py
+++ src/ogdrb/main.py
@@ -27,19 +27,49 @@ async def index() -> None:
         },
     ).classes("w-full h-96")
 
+    # programmatic circle
+    # https://github.com/zauberzeug/nicegui/discussions/4644
+    async def add_circle() -> None:
+        id_ = await ui.run_javascript(
+            f"""
+            const out = [];
+            const map = getElement('{m.id}').map;
+            map.eachLayer(layer => {{
+                if (layer instanceof L.FeatureGroup) {{
+                    const myCircle = L.circle(
+                        [51.49, -0.095], {{ radius: 200 }}
+                    ).addTo(layer);
+                    out.push(myCircle);
+                }}
+            }});
+            return L.stamp(out[0]);
+            """,
+            timeout=1.0,
+        )
+        ui.notify(f"Programmatic circle added (id: {id_})")
+
     async def list_circles() -> None:
-        circles = await ui.run_javascript(f"""
+        circles = await ui.run_javascript(
+            f"""
             const out = [];
             getElement('{m.id}').map.eachLayer(layer => {{
                 if (layer instanceof L.Circle) {{
                     const c = layer.getLatLng();
-                    out.push({{ id: L.stamp(layer), lat: c.lat, lng: c.lng, radius: layer.getRadius() }});
+                    out.push({{
+                        id: L.stamp(layer),
+                        lat: c.lat,
+                        lng: c.lng,
+                        radius: layer.getRadius()
+                    }});
                 }}
             }});
             return out;
-        """)
+            """,
+            timeout=1.0,
+        )
         ui.notify(f"All circles: {circles}")
 
+    ui.button("Add circle", on_click=add_circle)
     ui.button("List all circles", on_click=list_circles)
 
 
