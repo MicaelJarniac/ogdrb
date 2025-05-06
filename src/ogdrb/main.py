c7737e0369477ae544efa3c018e398866fbd84f6
Micael Jarniac <micael@jarniac.dev>
2025-05-05 21:50:39 -0300

    feat: sync table and map the other way around

--- src/ogdrb/main.py
+++ src/ogdrb/main.py
@@ -22,7 +22,7 @@ class Circle(TypedDict):
 
 
 class ZoneRow(TypedDict):
-    """ZoneRow class for AgGrid."""
+    """ZoneRow class for AG Grid."""
 
     id: int
     name: str
@@ -34,6 +34,8 @@ class ZoneRow(TypedDict):
 @ui.page("/")
 async def index() -> None:
     rows: list[ZoneRow] = []
+
+    circles_to_zones: dict[int, int] = {}
 
     # Leaflet map with circle-only draw toolbar
     m = ui.leaflet(
@@ -53,9 +55,10 @@ async def index() -> None:
         },
     ).classes("w-full h-96")
 
-    # Programmatic circle
-    # https://github.com/zauberzeug/nicegui/discussions/4644
-    async def add_circle(lat: float, lng: float, radius: float) -> int:
+    async def add_circle(
+        lat: float, lng: float, radius: float, *, selected: bool = False
+    ) -> int:
+        # https://github.com/zauberzeug/nicegui/discussions/4644
         id_: int = await ui.run_javascript(
             f"""
             const out = [];
@@ -63,7 +66,11 @@ async def index() -> None:
             map.eachLayer(layer => {{
                 if (layer instanceof L.FeatureGroup) {{
                     const myCircle = L.circle(
-                        [{lat}, {lng}], {{ radius: {radius} }}
+                        [{lat}, {lng}],
+                        {{
+                            radius: {radius},
+                            color: '{"red" if selected else "blue"}',
+                        }}
                     ).addTo(layer);
                     out.push(myCircle);
                 }}
@@ -72,51 +79,13 @@ async def index() -> None:
             """,
             timeout=1.0,
         )
-        ui.notify(f"Programmatic circle added (id: {id_})")
         return id_
 
-    async def list_circles() -> list[Circle]:
-        circles: list[Circle] = await ui.run_javascript(
+    async def delete_all_circles() -> None:
+        await ui.run_javascript(
             f"""
-            const out = [];
             getElement('{m.id}').map.eachLayer(layer => {{
                 if (layer instanceof L.Circle) {{
-                    const c = layer.getLatLng();
-                    out.push({{
-                        id: L.stamp(layer),
-                        lat: c.lat,
-                        lng: c.lng,
-                        radius: layer.getRadius()
-                    }});
-                }}
-            }});
-            return out;
-            """,
-            timeout=1.0,
-        )
-        ui.notify(f"All circles: {circles}")
-        return circles
-
-    async def edit_circle(id_: int, lat: float, lng: float, radius: float) -> None:
-        await ui.run_javascript(
-            f"""
-            getElement('{m.id}').map.eachLayer(layer => {{
-                if (layer instanceof L.Circle && L.stamp(layer) === {id_}) {{
-                    layer.setLatLng([{lat}, {lng}]);
-                    layer.setRadius({radius});
-                }}
-            }});
-            return;
-            """,
-            timeout=1.0,
-        )
-        ui.notify(f"Circle edited (id: {id_})")
-
-    async def delete_circle(id_: int) -> None:
-        await ui.run_javascript(
-            f"""
-            getElement('{m.id}').map.eachLayer(layer => {{
-                if (layer instanceof L.Circle && L.stamp(layer) === {id_}) {{
                     layer.remove();
                 }}
             }});
@@ -124,75 +93,131 @@ async def index() -> None:
             """,
             timeout=1.0,
         )
-        ui.notify(f"Circle deleted (id: {id_})")
 
-    default_columns = {
-        "sortable": False,
-    }
+    async def sync_circles() -> None:
+        """Sync circles with the map."""
+        await delete_all_circles()
+        circles_to_zones.clear()
+        selected = await aggrid.get_selected_rows()
+        selected_ids = [row["id"] for row in selected]
+        for row in rows:
+            circles_to_zones[
+                await add_circle(
+                    lat=row["lat"],
+                    lng=row["lng"],
+                    radius=row["radius"] * 1000,  # convert km to m
+                    selected=row["id"] in selected_ids,
+                )
+            ] = row["id"]
+
+    async def draw_created(e: GenericEventArguments) -> None:
+        layer = e.args.get("layer")
+        if not layer:
+            return
+        center = layer["_latlng"]
+        radius = layer["_mRadius"]
+        rows.append(
+            ZoneRow(
+                id=new_id(),
+                name="New Zone",
+                lat=center["lat"],
+                lng=center["lng"],
+                radius=radius / 1000,  # convert m to km
+            )
+        )
+        aggrid.update()
+        await sync_circles()
+
+    async def draw_edited(e: GenericEventArguments) -> None:
+        layers = e.args.get("layers")
+        if not layers:
+            return
+        for layer in layers["_layers"].values():
+            row_id = circles_to_zones.get(layer["_leaflet_id"])
+            if not row_id:
+                ui.notify(f"Circle with ID {layer['_leaflet_id']} not found")
+                continue
+            row = next((row for row in rows if row["id"] == row_id), None)
+            if row:
+                ui.notify(row)
+                center = layer["_latlng"]
+                radius = layer["_mRadius"]
+                ui.notify(radius)
+                row["lat"] = center["lat"]
+                row["lng"] = center["lng"]
+                row["radius"] = radius / 1000
+                aggrid.update()
+                await sync_circles()
+
+    async def draw_deleted(e: GenericEventArguments) -> None:
+        layers = e.args.get("layers")
+        if not layers:
+            return
+        for layer in layers["_layers"].values():
+            row_id = circles_to_zones.get(layer["_leaflet_id"])
+            if not row_id:
+                ui.notify(f"Circle with ID {layer['_leaflet_id']} not found")
+                continue
+            row = next((row for row in rows if row["id"] == row_id), None)
+            if row:
+                rows.remove(row)
+                aggrid.update()
+                await sync_circles()
+
+    m.on("draw:created", draw_created)
+    m.on("draw:edited", draw_edited)
+    m.on("draw:deleted", draw_deleted)
+
+    def new_id() -> int:
+        """Get the next row ID."""
+        return max((row["id"] for row in rows), default=0) + 1
+
+    async def add_row() -> None:
+        rows.append(ZoneRow(id=new_id(), name="New Zone", lat=0.0, lng=0.0, radius=1.0))
+        ui.notify("Added row")
+        aggrid.update()
+        await sync_circles()
+
+    async def handle_cell_value_change(e: GenericEventArguments) -> None:
+        new_row: ZoneRow = e.args["data"]
+        ui.notify(f"Updated row to: {e.args['data']}")
+        rows[:] = [row | new_row if row["id"] == new_row["id"] else row for row in rows]
+        aggrid.update()
+        await sync_circles()
+
+    async def delete_selected() -> None:
+        selected_names = [row["id"] for row in await aggrid.get_selected_rows()]
+        rows[:] = [row for row in rows if row["id"] not in selected_names]
+        ui.notify(f"Deleted row with ID {selected_names}")
+        aggrid.update()
+        await sync_circles()
 
     columns = [
         {"field": "name", "headerName": "Name", "editable": True},
         {"field": "lat", "headerName": "Latitude", "editable": True},
         {"field": "lng", "headerName": "Longitude", "editable": True},
         {"field": "radius", "headerName": "Radius (km)", "editable": True},
-        {"field": "id", "headerName": "ID"},
+        {"field": "id", "headerName": "ID", "hide": True},
     ]
-
-    async def sync_rows() -> None:
-        """Sync rows with circles on the map."""
-        circles = await list_circles()
-        rows[:] = [
-            ZoneRow(
-                id=circle["id"],
-                name=f"Zone {circle['id']}",
-                lat=circle["lat"],
-                lng=circle["lng"],
-                radius=circle["radius"] / 1000.0,  # convert to km
-            )
-            for circle in circles
-        ]
-        aggrid.update()
-
-    m.on("draw:created", sync_rows)
-    m.on("draw:edited", sync_rows)
-    m.on("draw:deleted", sync_rows)
-
-    async def add_row() -> None:
-        new_id = await add_circle(lat=0.0, lng=0.0, radius=0.0)
-        ui.notify(f"Added row with ID {new_id}")
-        await sync_rows()
-
-    async def handle_cell_value_change(e: GenericEventArguments) -> None:
-        new_row: ZoneRow = e.args["data"]
-        ui.notify(f"Updated row to: {e.args['data']}")
-        await edit_circle(
-            id_=new_row["id"],
-            lat=new_row["lat"],
-            lng=new_row["lng"],
-            radius=new_row["radius"] * 1000.0,  # convert to m
-        )
-        await sync_rows()
-
-    async def delete_selected() -> None:
-        selected_id = [row["id"] for row in await aggrid.get_selected_rows()]
-        for id_ in selected_id:
-            await delete_circle(id_)
-        ui.notify(f"Deleted row with ID {selected_id}")
-        await sync_rows()
 
     aggrid = ui.aggrid(
         {
-            "defaultColDef": default_columns,
+            "defaultColDef": {
+                "sortable": False,
+            },
             "columnDefs": columns,
             "rowData": rows,
             "rowSelection": "multiple",
             "stopEditingWhenCellsLoseFocus": True,
-        }
-    ).on("cellValueChanged", handle_cell_value_change)
+        },
+        theme="balham-dark",
+    )
+    aggrid.on("cellValueChanged", handle_cell_value_change)
+    aggrid.on("rowSelected", sync_circles)
 
     ui.button("Delete selected zones", on_click=delete_selected)
     ui.button("New zone", on_click=add_row)
 
 
 if __name__ in {"__main__", "__mp_main__"}:
-    ui.run(title="ogdrb", storage_secret="ogdrb")  # noqa: S106
+    ui.run(title="ogdrb", storage_secret="ogdrb", dark=True)  # noqa: S106
